f57b25cf04f22b718f34d6366b2b28c342fed03e
Vincer25 <59052245+Vincer25@users.noreply.github.com>
2020-11-25 17:11:52 +0100

    Update WaveManager.cs

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -4,11 +4,11 @@ using UnityEngine.UI;
 
 public class WaveManager : MonoBehaviour
 {
-
     public List<GameObject> spawnitems = new List<GameObject>();
     public List<GameObject> spawners = new List<GameObject>();
     Text mytext;
     int waveNumber = 0;
+    int i;
 
     void Start()
     {
@@ -23,24 +23,33 @@ public class WaveManager : MonoBehaviour
         }
     }
 
-    public void current_wave()
-    {
-    mytext.text = waveNumber.ToString();
-    }
-
     public void next_wave()
     {
         waveNumber++;
-        current_wave();
+        mytext.text = waveNumber.ToString();
         spawn_wave();
     }
+
     public void spawn_wave()
     {
         for (int i = 0; i < spawners.Count; i++)
         {
-            Instantiate(spawnitems[0], spawners[i].transform.position + new Vector3(0,5,0), spawners[i].transform.rotation);
+            spawn_mob1();
         }
-        
     }
 
+    public void spawn_mob1()
+    {
+            Instantiate(spawnitems[0], spawners[i].transform.position + new Vector3(0, 0, -10), spawners[i].transform.rotation);
+    }
+
+    public void spawn_mob2()
+    {
+            Instantiate(spawnitems[1], spawners[i].transform.position + new Vector3(0, 0, -10), spawners[i].transform.rotation);
+    }
+
+    public void spawn_mob3()
+    {
+            Instantiate(spawnitems[2], spawners[i].transform.position + new Vector3(0, 0, -10), spawners[i].transform.rotation);
+    }
 }
